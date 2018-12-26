c8d8ffed69aaecb89a1c674c065aa8b505bc2284
Kento-Izumi <kento.i.1103@gmail.com>
2018-12-27 01:38:15 +0900

    add 初期機能追加
    ランチャーアプリの設定
    タイトルバー、ステータスバー、ナビゲーションバーの非表示
    背景色の変更

--- app/src/main/java/com/example/kenshirou/zerolauncher/HomeActivity.java
+++ app/src/main/java/com/example/kenshirou/zerolauncher/HomeActivity.java
@@ -2,6 +2,7 @@ package com.example.kenshirou.zerolauncher;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +10,17 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        stickyImmersiveMode();
+
+    }
+
+    private void stickyImmersiveMode(){
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        );
     }
 }
